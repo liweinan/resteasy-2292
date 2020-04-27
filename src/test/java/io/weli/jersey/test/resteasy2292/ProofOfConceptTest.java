df9aa7cc90332005f34bf7fc8eeff467cf13f6d1
阿男 <l.weinan@gmail.com>
2020-04-27 18:05:21 +0800

    test

---
+++ src/test/java/io/weli/jersey/test/resteasy2292/ProofOfConceptTest.java
@@ -0,0 +1,12 @@
+package io.weli.jersey.test.resteasy2292;
+
+
+import org.junit.Test;
+
+public class ProofOfConceptTest {
+
+    @Test
+    public void testFoo() {
+        System.out.println("Test");
+    }
+}
